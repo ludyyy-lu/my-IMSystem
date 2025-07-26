fc189230d35a7fb8203d30660c7cd9b7a138c2c8
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-07-26 14:41:18 +0800

    利用脚手架生成user服务的骨架

---
+++ user-service/user/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "my-IMSystem/user-service/user/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
