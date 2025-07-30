6b8835bafa52f501c67055b6a86afa47332fabf3
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-07-30 15:46:09 +0800

    修改部分配置信息

--- ws-gateway/internal/config/config.go
+++ ws-gateway/internal/config/config.go
@@ -4,4 +4,14 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
+	Redis struct {
+        Addr     string
+        Password string
+        DB       int
+    }
+
+    Kafka struct {
+        Brokers []string
+        Topic   string
+    }
 }
