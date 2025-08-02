04a9680aecc186eecaa3380a0f9dea6e00b580ae
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-08-02 14:42:22 +0800

    定义好友事件消息结构（所有服务共享）

---
+++ common/model/friend_event.go
@@ -0,0 +1,20 @@
+package model
+
+type FriendEventType string
+
+const (
+	FriendRequestReceived FriendEventType = "FriendRequestReceived"
+	FriendRequestAccepted FriendEventType = "FriendRequestAccepted"
+	FriendRequestRejected FriendEventType = "FriendRequestRejected"
+	FriendDeleted         FriendEventType = "FriendDeleted"
+	UserBlocked           FriendEventType = "UserBlocked"
+	UserUnblocked         FriendEventType = "UserUnblocked"
+)
+
+type FriendEvent struct {
+	EventType FriendEventType `json:"event_type"`
+	FromUser  int64           `json:"from_user"`
+	ToUser    int64           `json:"to_user"`
+	Timestamp int64           `json:"timestamp"`
+	Extra     string          `json:"extra"` // 可以放验证消息等
+}
