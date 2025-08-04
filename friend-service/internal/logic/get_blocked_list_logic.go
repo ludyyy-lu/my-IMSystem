d08b73e5aa328cbb3e790263c949444c0adc0859
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-08-04 15:59:49 +0800

    logic：取消拉黑、查看拉黑列表

--- friend-service/internal/logic/get_blocked_list_logic.go
+++ friend-service/internal/logic/get_blocked_list_logic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
-	"my-IMSystem/friend-service/friend"
+	friend_friend "my-IMSystem/friend-service/friend"
+	"my-IMSystem/friend-service/internal/model"
 	"my-IMSystem/friend-service/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type GetBlockedListLogic struct {
@@ -25,6 +27,19 @@ func NewGetBlockedListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Ge
 
 func (l *GetBlockedListLogic) GetBlockedList(in *friend_friend.GetBlockedListReq) (*friend_friend.GetBlockedListResp, error) {
 	// todo: add your logic here and delete this line
+	var list []model.BlockedUser
+	err := l.svcCtx.DB.Where("user_id = ?", in.UserId).Find(&list).Error
+	if err != nil {
+		l.Logger.Errorf("查询拉黑列表失败: %v", err)
+		return nil, status.Error(500, "获取拉黑列表失败")
+	}
+	// 提取 blocked_id 字段
+	var ids []int64
+	for _, bu := range list {
+		ids = append(ids, bu.BlockedID)
+	}
 
-	return &friend_friend.GetBlockedListResp{}, nil
+	return &friend_friend.GetBlockedListResp{
+		BlockedIds: ids,
+	}, nil
 }
--- friend-service/internal/logic/unblock_friend_logic.go
+++ friend-service/internal/logic/unblock_friend_logic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
-	"my-IMSystem/friend-service/friend"
+	friend_friend "my-IMSystem/friend-service/friend"
+	"my-IMSystem/friend-service/internal/model"
 	"my-IMSystem/friend-service/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type UnblockFriendLogic struct {
@@ -25,6 +27,11 @@ func NewUnblockFriendLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Unb
 
 func (l *UnblockFriendLogic) UnblockFriend(in *friend_friend.UnblockFriendReq) (*friend_friend.UnblockFriendResp, error) {
 	// todo: add your logic here and delete this line
-
-	return &friend_friend.UnblockFriendResp{}, nil
+	db := l.svcCtx.DB
+	err := db.Where("user_id = ? AND blocked_id = ?", in.UserId, in.TargetId).Delete(&model.BlockedUser{}).Error
+	if err != nil {
+		l.Logger.Errorf("Failed to unblock user: %v", err)
+		return nil, status.Error(500, "取消拉黑失败")
+	}
+	return &friend_friend.UnblockFriendResp{Msg: "取消拉黑成功"}, nil
 }
