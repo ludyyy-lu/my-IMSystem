44e043cd81a524a769208b9e966eda7da1bb5ecb
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-08-05 11:27:19 +0800

    logic:设备登出

--- auth-service/internal/logic/logout_session_logic.go
+++ auth-service/internal/logic/logout_session_logic.go
@@ -4,9 +4,12 @@ import (
 	"context"
 
 	auth_auth "my-IMSystem/auth-service/auth"
+	"my-IMSystem/auth-service/internal/model"
 	"my-IMSystem/auth-service/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type LogoutSessionLogic struct {
@@ -25,7 +28,13 @@ func NewLogoutSessionLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Log
 
 // 多设备：注销某个设备的 token（退出登录）
 func (l *LogoutSessionLogic) LogoutSession(in *auth_auth.LogoutSessionReq) (*auth_auth.LogoutSessionResp, error) {
-	// todo: add your logic here and delete this line
+	// 尝试删除该用户某个设备的 Session
+	err := l.svcCtx.DB.Where("uid = ? AND device_id = ?", in.UserId, in.DeviceId).Delete(&model.Session{}).Error
+	if err != nil {
+		return nil, status.Error(codes.Internal, "注销设备失败: "+err.Error())
+	}
 
-	return &auth_auth.LogoutSessionResp{}, nil
+	return &auth_auth.LogoutSessionResp{
+		Message: "设备登出成功",
+	}, nil
 }
