9afe53b95d09911597b4808b3bd7dc188d86b9dd
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-08-05 20:39:36 +0800

    user:获取用户信息

--- user-service/internal/logic/get_profile_logic.go
+++ user-service/internal/logic/get_profile_logic.go
@@ -7,6 +7,8 @@ import (
 	user_user "my-IMSystem/user-service/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type GetProfileLogic struct {
@@ -25,6 +27,20 @@ func NewGetProfileLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetPro
 
 func (l *GetProfileLogic) GetProfile(in *user_user.GetProfileReq) (*user_user.GetProfileResp, error) {
 	// todo: add your logic here and delete this line
+	u, err := l.svcCtx.UserModel.FindByID(l.ctx, in.Uid)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "用户不存在")
+	}
 
-	return &user_user.GetProfileResp{}, nil
+	return &user_user.GetProfileResp{
+		User: &user_user.UserInfo{
+			Id:        u.ID,
+			Nickname:  u.Nickname,
+			Avatar:    u.Avatar,
+			Bio:       u.Bio,
+			CreatedAt: u.CreatedAt.Unix(),
+			Disabled:  u.Disabled,
+			Gender:    u.Gender,
+		},
+	}, nil
 }
--- user-service/internal/svc/service_context.go
+++ user-service/internal/svc/service_context.go
@@ -1,13 +1,34 @@
 package svc
 
-import "my-IMSystem/user-service/internal/config"
+import (
+	"log"
+	"my-IMSystem/user-service/internal/config"
+	"my-IMSystem/user-service/internal/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	DB        *gorm.DB         // 数据库连接
+	UserModel *model.UserModel // 数据库访问接口
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+
+	db, err := gorm.Open(mysql.Open(c.Mysql.DataSource), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect DB: %v", err)
+	}
+	// 自动建表
+	err = db.AutoMigrate(&model.User{})
+	if err != nil {
+		log.Fatalf("auto migration failed: %v", err)
+	}
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		DB:        db,
+		UserModel: model.NewUserModel(db),
 	}
 }
