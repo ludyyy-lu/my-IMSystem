147d213a4439e358ffebb61d961f16e1be3ed4ad
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-08-05 10:25:38 +0800

    logic:验证token是否有效

--- auth-service/internal/logic/verify_token_logic.go
+++ auth-service/internal/logic/verify_token_logic.go
@@ -5,6 +5,7 @@ import (
 
 	auth_auth "my-IMSystem/auth-service/auth"
 	"my-IMSystem/auth-service/internal/svc"
+	"my-IMSystem/pkg/jwt"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -26,6 +27,19 @@ func NewVerifyTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Verif
 // 校验 token 是否有效
 func (l *VerifyTokenLogic) VerifyToken(in *auth_auth.VerifyTokenReq) (*auth_auth.VerifyTokenResp, error) {
 	// todo: add your logic here and delete this line
+	claims, err := jwt.ParseToken(in.AccessToken, l.svcCtx.Config.JwtAuth.AccessSecret)
+	if err != nil {
+		// Token 无效，返回 valid = false
+		return &auth_auth.VerifyTokenResp{
+			Valid:    false,
+			UserId:   0,
+			DeviceId: "",
+		}, nil
+	}
 
-	return &auth_auth.VerifyTokenResp{}, nil
+	return &auth_auth.VerifyTokenResp{
+		Valid:    true,
+		UserId:   claims.Uid,
+		DeviceId: claims.DeviceId,
+	}, nil
 }
